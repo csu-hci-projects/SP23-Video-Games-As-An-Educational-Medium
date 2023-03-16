808d4ba90b6ad40f5fc178629192a08af5102ee8
Jenelle Dobyns <jenelledobyns@gmail.com>
2023-03-16 14:51:33 -0600

    Initial navbar.js content

---
+++ components/Navbar.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components';
+import Link from 'next/link';
+
+const Nav = styled.nav`
+	height: 80px;
+	background: #fefefe;
+	color: #0b0b0b;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`
+
+const StyledLink = styled.a`
+	padding: 0rem 2rem;
+`
+
+const StyledUl = styled.ul`
+	list-style-type: none;
+	display: inline-flex;
+	`
+
+const Navbar = () => {
+  return (
+	<Nav>
+		<div>	
+			<StyledLink href="/">
+				GEO
+			</StyledLink>
+		</div>
+		<div>
+			<StyledUl>
+				<li>
+					<StyledLink href="/">
+						Home
+					</StyledLink>
+				</li>
+				<li>
+					<StyledLink href="/">
+						About
+					</StyledLink>
+				</li>
+				<li>
+					<StyledLink href="/">
+						Contact
+					</StyledLink>
+				</li>
+			</StyledUl>
+		</div>
+	</Nav>
+  )
+}
+
+export default Navbar
