99fea1549c78c65f780dfa29e8f25e5fbefe02de
Jenelle Dobyns <jenelledobyns@gmail.com>
2023-03-16 16:27:39 -0600

    revert deployment stuff
    add export command
    
    setup automatic deployment 1
    
    Create static.yml
    
    update deployment action 1
    
    build and deploy 2
    
    Delete static.yml
    
    update deploy action version
    
    Update build-and-deploy.yml
    
    Update next.config.js
    
    Update next.config.js
    
    Create nodejs.yml
    
    delete old deployment stuff
    
    update webpack
    
    Update next.config.mjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import Layout from '@/components/Layout'
+import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
-import '@/styles/main.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <Component className={inter.className} {...pageProps} />
     </Layout>
   )
 }
