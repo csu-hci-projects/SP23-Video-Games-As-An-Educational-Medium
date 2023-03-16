3cf28782013f89e04264fa7fe64af79b3cfbdef1
Jenelle Dobyns <jenelledobyns@gmail.com>
2023-03-16 16:40:05 -0600

    setup automatic deployment 1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${geopursuit}/`
+  basePath = `/${geopursuit}`
 }
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
