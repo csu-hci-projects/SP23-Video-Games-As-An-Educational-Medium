9363c3162e8cf1c8ae117ab61aa8e77a07f6b5a9
Samuel Mckay <89430082+Retrope13@users.noreply.github.com>
2023-03-18 21:49:58 -0600

    make footer stick to bottom of page

--- components/Footer.js
+++ components/Footer.js
@@ -3,10 +3,13 @@ import styled from 'styled-components'
 const FooterSection = styled.div`
 background: #fff;
 color: #202733;
-height: 50px;
+height: 40px;
+min-width: 100%;
 display: flex;
 justify-content: center;
-align-items: center;`
+align-items: center;
+position: relative;
+`
 
 const Footer = () => {
   return (
