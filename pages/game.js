743a675a01a6ba5ab704a427fc5b689d26106393
Samuel Mckay <89430082+Retrope13@users.noreply.github.com>
2023-03-20 21:45:26 -0600

    add dropdown and options for dropdown

--- pages/game.js
+++ pages/game.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import styles from '@/styles/Home.module.css';
 import styled from 'styled-components';
 import samImg from "public/SamMcKay.jpg";
+import Dropdown from "/components/Dropdown";
 
 const Hero = styled.div`
   height: 80vh;
@@ -10,57 +11,58 @@ const Hero = styled.div`
   justify-content: start;
   align-items: flex-start;
   background: #202733;
-  height: 100%;
-  `
-
-  const Heading = styled.h1`
-    color: #fff;
-    font-size: 6rem;
-    font-weight: 900;`
-
-  const Heading2 = styled.h2`
-    color: #fff;
-    font-size: 40px;
-    font-weight: 500;
-    margin-left: 3%;
-    margin-top: 2%;
-    margin-bottom: 2%;
-    display: block;
   `
 
 const StyledButton = styled.button`
   font-size: 15px;
-  font-weight: 20px;
-  background-color: #2b3547;
-  margin-left: 15%;
-  margin-bottom: 5%;
-  border-color: #2b3547;
-  width: 350px;
-  height: 450px;
-  text-align: left;
-  background-image: url('public/SamMcKay.jpg');
+  background-color: #202733;
+  margin-left: 40%;
+  border-color: #fff;
+  width: 20%;
+  height: 10%;
+  border-radius: 5px;
+  margin-bottom: 3%;
 `
 
 const StyledText = styled.p`
-  margin-left: 5%;
+  margin-left: 10%;
   font-size: 15px;
   max-width: 80%;
 
 `
 
-const StyledLink = styled.a`
-	padding: 0rem 2rem;
-	color: #fff;
-	font-size: 20px;
-`
-
 const StyledImage = styled.img`
   height: 200px;
   width: 250px;
+  margin-top: 5%;
   margin-bottom: 2%;
-  margin-left: 15%;
-  postion: top;
+  postion: static;
+  margin-left: 40%;
+  display: block;
 `
+
+const options = [
+  { value: "Algeria", label: "Algeria" }, 
+  { value: "Angola", label: "Angola"},
+  { value: "Benin", label: "Benin"},
+  { value: "Botswana", label: "Botswana"},
+  { value: "Burkina Faso", label: "Burkina Faso"},
+  { value: "Burundi", label: "Burundi"},
+  { value: "Cameroon", label: "Cameroon"},
+  { value: "Cabo Verde", label: "Cabo Verde"},
+  { value: "Central African Republic", label: "Central African Republic"},
+  { value: "Chad", label: "Chad"},
+  { value: "Comoros", label: "Comoros"},
+  { value: "Côte dIvoire", label: "Côte dIvoire"},
+  { value: "Democratic Republic of the Congo", label: "Democratic Republic of the Congo"},
+  { value: "Djibouti", label: "Djibouti"},
+  { value: "Egypt", label: "Egypt"},
+  { value: "Equatorial Guinea", label: "Equatorial Guinea"},
+  { value: "Eritrea", label: "Eritrea"},
+  { value: "Eswatini", label: "Eswatini"},
+  { value: "Ethiopia", label: "Ethiopia"},
+
+]
 
 export default function Home() {
   return (
@@ -72,26 +74,13 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
       </Head>
       <Hero>
-        <main>
-          <Heading2>About Us</Heading2>
-          <StyledButton><StyledImage src={samImg} alt='A photo of Sam McKay at Meow Wolf'/><br/>
-            <StyledText>Hello my name is Sam</StyledText>
-          </StyledButton>
-
-          <StyledButton><StyledImage src='/Images/About_images/SamMcKay.jpg'/><br/>
-            <StyledText>Hello my name is Sam</StyledText>
-          </StyledButton>
-
-          <StyledButton><StyledImage src='pages/Images/About_images/SamMcKay.jpg'/><br/>
-            <StyledText>Hello my name is Sam</StyledText>
-          </StyledButton>
-
-          <StyledButton><StyledImage src='pages/Images/About_images/SamMcKay.jpg'/><br/>
-            <StyledText>Hello my name is Sam</StyledText>
-          </StyledButton>
-
-          <br/>
-        </main>
+        <StyledImage src = "/Outlines/Algeria.png"/>
+      </Hero>
+      <Hero>
+        <Dropdown isSearchable placeHolder="Select..." options={options}/>
+      </Hero>
+      <Hero>
+        <StyledButton><StyledText>Submit Answer</StyledText></StyledButton>
       </Hero>
     </>
   )
